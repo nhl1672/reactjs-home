4838dcae6e8e0d14382ad6d09b6e4b459ff0b65a
LongNh <nhlong1672@gmail.com>
2022-09-26 13:36:50 +0700

    Code data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Lifecycle from './Components/Lifecycle/lifecycle';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 function App() {
   return (
     <div>
